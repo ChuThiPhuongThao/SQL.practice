f156259a80a872fe097154eea5e5814d1f8198e0
ChuThiPhuongThao <156778305+ChuThiPhuongThao@users.noreply.github.com>
2024-02-17 13:29:50 +0700

    Update day21.sql

--- day21.sql
+++ day21.sql
@@ -10,7 +10,7 @@ order by month_year
 /* Nhận xét:
 1. Số lượng đơn hàng và người mua tăng dần theo thời gian
 2. Trong mỗi năm, số lượng đơn hàng và người mua đều tăng dần theo các tháng và đạt đỉnh vào 3 tháng cuối năm
-3. Số lượng khách mua nhiều đơn trong 1 tháng hầu như không đang kể và vẫn chưa có dấu hiệu tăng trưởng
+3. Số lượng khách mua nhiều đơn trong 1 tháng hầu như không đang kể và vẫn chưa có dấu hiệu tăng trưởng*/
 
 --- task 2:
 select 
@@ -23,6 +23,11 @@ on t1.order_id = t2.order_id
 where t1.created_at between '2019-01-01' and '2022-05-01'
 group by month_year
 order by month_year
+/* Nhận xét:
+1. SỐ lương người mua hàng đã có sự tăng dần và tăng trưởng ổn định theo thời gian 
+2. Giá trị trung bình đơn hàng trong khoảng thời gian theo dõi nhìn chung đã có gia tăng, nhưng dao động ổn định ở quanh mốc 110 trong phần lớn các tháng
+
+
 
 
 
